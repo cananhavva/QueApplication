3670fe6f2f78b600cd2e54b45959907186cbb35e
cananhavva <cananhavvaarslan@gmail.com>
2022-01-15 04:04:46 +0300

    comment conroller

--- src/main/java/com/cananarslan/queapplication/controller/CommentController.java
+++ src/main/java/com/cananarslan/queapplication/controller/CommentController.java
@@ -1,5 +1,17 @@
 package com.cananarslan.queapplication.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cananarslan.queapplication.services.CommentServices;
+
+@RestController
+@RequestMapping("/comments")
 public class CommentController {
+	private CommentServices commentServices;
+	
+	public CommentController(CommentServices commentServices) {
+		this.commentServices = commentServices;
+	}
 	
 }
